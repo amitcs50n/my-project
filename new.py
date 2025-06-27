12b03d615d99d1ceb3d8825aa485b0207740f6c5
Amit <ay5097502@gmail.com>
2025-06-27 20:39:49 +0530

    Updated file with new logic/comment

--- new.py
+++ new.py
@@ -6,3 +6,11 @@ import pandas as pd
 
 print(pd.__version__)
 
+
+# map , lambda, reduce 4
+
+seq = [1, 2, 3, 4, 5, 6, 7, 8]
+
+# find even numbers
+even_num = [y for y in seq if y % 2 == 0]
+print(even_num)
